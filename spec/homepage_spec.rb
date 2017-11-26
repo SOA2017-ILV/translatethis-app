7b2d548e2b4068e3a9a84ac6f2bb808cb81db84b
ismael-laptop <ismaelnoble@gmail.com>
2017-11-26 23:57:45 +0800

    moved around tests and added some flash bar tests"

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -26,9 +26,6 @@ describe 'Homepage' do
       _(@browser.file_field(name: 'img').visible?).must_equal true
       _(@browser.select_list(name: 'target_lang').visible?).must_equal true
       _(@browser.button(text: 'Translate This!').visible?).must_equal true
-
-      #_(@browser.div(id: 'flash_bar_success').visible?).must_equal true #TODO uncomment this after flashbar is done
-      #_(@browser.div(id: 'flash_bar_success').text).must_include 'something' #TODO uncomment this after flashbar is done
     end
   end
 
@@ -41,25 +38,23 @@ describe 'Homepage' do
       @browser.file_field(:name, 'img').set(IMAGE)
       @browser.select_list(:name, 'target_lang').option(:value, 'es').select
       @browser.button(:text, 'Translate This!').click
-      # _(@browser.div(id: 'flash_bar_success').text).must_include 'Translate This Success' # TODO: include when flash done
-      # _(@browser.div(id: 'flash_bar_danger').exists?).must_equal false # TODO: include when flash done
-
       # THEN: user should see their new translation
-      _@browser.html.must_include 'Your picture was recognized'
-      # TODO: change to reflect updated ui
+      _(@browser.div(id: 'flash_bar_success').visible?).must_equal true
+      _(@browser.div(id: 'flash_bar_success').text)
+        .must_include 'Translate Sucess'
+      # TODO: add more checks to reflect updated ui
     end
 
     it '(BAD) should not accept incorrect filetype' do
       # GIVEN: user is on the home page
       @browser.goto homepage
-
       # WHEN: user enters an invalid URL
       @browser.file_field(:name, 'img').set(BADFILE)
       @browser.select_list(:name,'target_lang').option(:value, 'es').select
       @browser.button(:text, 'Translate This!').click
-
       # THEN: user should see an error alert and no table of repos
-      _(@browser.div(id: 'flash_bar_danger').text).must_include 'Invalid'
+       _(@browser.div(id: 'flash_bar_danger').text).must_include 'Failure("No image found")'
+       _(@browser.div(id: 'flash_bar_danger').exists?).must_equal true
     end
   end
 end
