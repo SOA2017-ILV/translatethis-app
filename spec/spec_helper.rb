8babd3dbab3ee576b691de9e62283cd990815511
ismael <ismaelnoble@gmail.com>
2017-11-29 13:11:03 +0800

    rebase conflics resolved

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,12 @@ require 'headless'
 
 require './init.rb'
 
+<<<<<<< HEAD
 require 'page-object'
 require_relative 'pages/init'
 
+=======
+>>>>>>> rebase conflics resolved
 HOST = 'http://localhost:3000'
 IMAGE = File.expand_path('./spec/fixtures/demo-image.jpg').freeze
 BADFILE = File.expand_path('./spec/fixtures/badfile.pdf').freeze
