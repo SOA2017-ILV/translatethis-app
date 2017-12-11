6e7094809b2c4913dd40726210b74eaa2b387fb9
ismaelnoble <ismaelnoble@gmail.com>
2017-12-11 11:26:45 +0800

    rebase8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,11 @@ require './init.rb'
 require 'page-object'
 require_relative 'pages/init'
 
+<<<<<<< HEAD
 require 'page-object'
 require_relative 'pages/init'
 
+>>>>>>> more rebase conflict resolution
 HOST = 'http://localhost:3000'
 IMAGE = File.expand_path('./spec/fixtures/demo-image.jpg').freeze
 BADFILE = File.expand_path('./spec/fixtures/badfile.pdf').freeze
